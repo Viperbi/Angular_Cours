5da392f78b0fac79b5b57a91a55642bf70e186bf
Viperbi <nickels.florian@gmail.com>
2025-02-11 14:07:56 +0100

    databinding suite

--- src/app/data-b/data-b.component.ts
+++ src/app/data-b/data-b.component.ts
@@ -1,16 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-b',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './data-b.component.html',
   styleUrl: './data-b.component.css',
 })
 export class DataBComponent {
   inputTexte: string = 'Ceci est du texte';
+  amiCreated: boolean = false;
+  ami: string = '';
 
-  onClick(event: any) {
+  onClick() {
+    this.ami = this.inputTexte;
     this.inputTexte = '';
+    this.amiCreated = true;
   }
 }
