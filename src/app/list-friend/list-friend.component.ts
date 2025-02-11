2e8884e2641e4e4c1944ca6b69244cec919afc26
Viperbi <nickels.florian@gmail.com>
2025-02-11 09:37:44 +0100

    exo event

--- src/app/list-friend/list-friend.component.ts
+++ src/app/list-friend/list-friend.component.ts
@@ -8,10 +8,19 @@ import { OneFriendComponent } from './one-friend/one-friend.component';
   styleUrl: './list-friend.component.css',
 })
 export class ListFriendComponent implements OnInit {
+  inputValue: string = '';
   boutton: boolean = false;
+  ami: string = 'Aucun Ami';
   ngOnInit(): void {
     setTimeout(() => {
       this.boutton = !this.boutton;
     }, 3000);
+  }
+
+  onKey(event: any) {
+    this.inputValue = event.target.value;
+  }
+  onClick(event: any) {
+    this.ami = 'Ami Ajouté';
   }
 }
